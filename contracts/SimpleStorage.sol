72e9ae015067520978bc4fd532af18321171f3c9
Formyselfonly <z1597006376@gmail.com>
2025-04-29 16:40:24 +0800

    update

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.20;
 
 contract SimpleStorage{
     uint256 public storedValue;
-    function set(uint256 _value) public{
+    function deposit(uint256 _value) public{
         storedValue=_value;
     }
 
-    function get() public view returns (uint256){
+    function get_balance() public view returns (uint256){
         return storedValue;
     }
 }
